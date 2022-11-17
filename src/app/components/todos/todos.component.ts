d1cded727124e854f78a675ac186beb345873655
Agustin Gomez <agusleongomez@gmail.com>
2022-11-17 13:02:12 -0300

    added todos and styles

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[] | undefined;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.todos = [
+      { content: 'First Todo', completed: false },
+      { content: 'Second Todo', completed: false },
+    ];
   }
-
 }
